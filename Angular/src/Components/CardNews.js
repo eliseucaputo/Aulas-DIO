632351647352a3083bf44d292c539075cf2f7d85
Eliseu Caputo <eliseujunior188@gmail.com>
2022-11-03 11:07:58 -0300

    site Darth Vader

---
+++ Angular/src/Components/CardNews.js
@@ -0,0 +1,51 @@
+class Cardnews extends HTMLElement {
+    constructor() {
+        super()
+
+        const shadow = this.attachShadow({ mode: "open" })
+        shadow.appendChild(this.build())
+        shadow.appendChild(this.styles())
+    }
+
+    build() {
+        const componentRoot = document.createElement("div")
+        componentRoot.setAttribute("class", "card")
+
+        const cardLeft = document.createElement("div")
+        cardLeft.setAttribute("class", "card-left")
+
+        const autor = document.createElement("span")
+        autor.textContent = "By " + (this.getAttribute("autor") || "Anonymous")
+
+        const linkTitle = document.createElement("a")
+        linkTitle.textContent = this.getAttribute("title")
+        linkTitle.href = this.getAttribute("link-url")
+
+        const newsContent = document.createElement("p")
+        newsContent.textContent = this.getAttribute("content")
+
+        cardLeft.appendChild(autor)
+        cardLeft.appendChild(linkTitle)
+        cardLeft.appendChild(newsContent)
+
+        const cardRight = document.createElement("div")
+        cardRight.setAttribute("class", "card-right")
+
+        const newsImage = document.createElement("img")
+        newsImage.src = this.getAttribute("photo") || "/assets/default-photo.jpg"
+        newsImage.alt = "Imagem do Darth Vader"
+        cardRight.appendChild(newsImage)
+
+
+        componentRoot.appendChild(cardLeft)
+        componentRoot.appendChild(cardRight)
+
+        return componentRoot
+    }
+
+    styles() {
+
+    }
+}
+
+customElements.define("card-news", Cardnews) 
